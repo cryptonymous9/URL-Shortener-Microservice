88f569a682a39d18cacb0289c5176c241380fd73
Nidhin Harilal <nidhin.harilal@iitgn.ac.in>
2019-03-24 03:03:33 +0530

    URL clash update

--- server.js
+++ server.js
@@ -15,35 +15,50 @@ app.use(cors());
 
 
 ////  connecting Mongodb
-mongoose.connect("mongodb+srv://nidhin:apple@cluster0-xuqnp.gcp.mongodb.net/test?retryWrites=true",{ useNewUrlParser: true }).catch((err)=>{
+mongoose.connect("mongodb+srv://nidhin:apple@cluster0-xuqnp.gcp.mongodb.net/test?retryWrites=true", {
+  useNewUrlParser: true
+}).catch((err) => {
   console.log(err);
 })
 
 //schemas
 var Schema = mongoose.Schema
 var shorturl_schema = new Schema({
-  url: {type:String, required:true},
-  index: Number,
-  new_url: {type:String},
+  url: {
+    type: String,
+    required: true
+  },
+  index: {
+    type: String
+  },
+  new_url: {
+    type: String
+  },
 });
 
-var Urls = mongoose.model('Urls',shorturl_schema)
+var Urls = mongoose.model('Urls', shorturl_schema)
 
 
 /** this project needs to parse POST bodies **/
 // you should mount the body-parser here
 var bodyParser = require('body-parser')
 //to use classic encoding
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
 
 //  Setting initial document
-app.get("/api/setinit/",(req,res)=>{
-  const link= new Urls({url:'https://www.google.com/',index:0,new_url:'/api/0'})
-  link.save((err,data)=>{
-    if(err) throw new Error((error))
+app.get("/api/setinit/", (req, res) => {
+  const link = new Urls({
+    url: 'https://www.google.com/',
+    index: 0,
+    new_url: '/api/0'
+  })
+  link.save((err, data) => {
+    if (err) throw new Error((err))
     console.log(data)
   })
   res.json(link)
@@ -55,32 +70,68 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-app.get('/api/:new/',(req,res,data)=>{
-
-    Urls.findOne({index: Number(req.params.new)}).then((data)=>{
-      res.redirect(data.url)  
-    }).catch((err)=>{
-      res.send("nothing found")
-    })
+app.get('/api/:new/', (req, res, data) => {
+  console.log(req.params.new)
+  Urls.findOne({
+    index: String(req.params.new)
+  }).then((data) => {
+    var redirect_link = data.url
+    if(data.url.substring(0,3)==='www'){redirect_link='https://'+data.url}
+    res.redirect(redirect_link)
+  }).catch((err) => {
+    console.log(err)
+    res.send("nothing found")
+  })
 })
 
-app.post('/api/shorturl/new',(req,res)=>{
+
+// looking up the posted URL using dns.lookup!!
+//generating random string and adding up it as the new lookup URL
+
+app.post('/api/shorturl/new', (req, res, next) => {
   var original_url = req.body.url
-  sliced_url = original_url
-  if(original_url.includes("://")) {
-    sliced_url = original_url.substring(8,)
+  var sliced_url = original_url
+  console.log("hello")
+  if (original_url.includes("://")) {
+    sliced_url = original_url.substring(8, )
     console.log(sliced_url)
   }
-  dns.lookup(sliced_url,(err,address)=>{
-    if(err){
+  // dns.lookup(sliced_url, (err, address) => {
+    var re = /^(http[s]?:\/\/){0,1}(www\.){0,1}[a-zA-Z0-9\.\-]+\.[a-zA-Z]{2,5}[\.]{0,1}/;
+    if (!re.test(original_url)) {
       console.log(err)
-      res.send(err)
+      res.json({
+        "error": "invalid URL"
+      })
     }
-    console.log(address)
-    res.json(address)
-  })
+    var flag = 0
+    do {
+      var random_string = Math.random().toString(36).substring(2, 6)
+      Urls.findOne({
+        index: random_string
+      }).then((key) => {
+        flag = 1
+      }).catch((err) => {
+        console.log(random_string)
+      })
+    } while (flag === 1)
 
- })
+    const link = new Urls({
+      url: original_url,
+      index: random_string,
+      new_url: '/api/' + random_string
+    })
+    link.save((err, data) => {
+      if (err) throw new Error(err)
+      console.log(data)
+    })
+    // console.log(address)
+    res.json({
+      "original_url": original_url,
+      "short_url": link.new_url
+    })
+  // })
+})
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
